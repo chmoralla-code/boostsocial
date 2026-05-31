87fd1b4e9421db762cbfe9a60e9f47706f383354
chmoralla-code <chmoralla-code@users.noreply.github.com>
2026-06-01 01:07:13 +0800

    Make simple mode toggle globally visible

--- src/components/FloatingSiteWidgets.tsx
+++ src/components/FloatingSiteWidgets.tsx
@@ -4,6 +4,7 @@ import Script from "next/script";
 import { usePathname } from "next/navigation";
 import { Chathead } from "@/components/Chathead";
 import { LiveOrderTicker } from "@/components/LiveOrderTicker";
+import { SimpleModeToggle } from "@/components/SimpleModeToggle";
 import { useSimpleMode } from "@/hooks/useSimpleMode";
 
 function isAdminSurface(pathname: string) {
@@ -21,6 +22,9 @@ export function FloatingSiteWidgets() {
         <>
           <Script src="https://js.puter.com/v2/" strategy={simpleMode ? "lazyOnload" : "afterInteractive"} />
           <Chathead />
+          <div className="fixed right-3 top-[76px] z-[95] sm:right-5 sm:top-24">
+            <SimpleModeToggle className="w-auto bg-[#181818]/95 shadow-lg shadow-black/20" />
+          </div>
         </>
       )}
       {!simpleMode && <LiveOrderTicker />}
